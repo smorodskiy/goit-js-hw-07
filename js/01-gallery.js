7d3c4e1ee2a5c1b1f23d9e901c7a0dc3f30799bd
Igor Smorodsky <ingerard@gmail.com>
2022-08-22 23:21:18 +0300

    done gallery 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,85 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+let imgBox;
+
+// Append all images from array to html
+galleryItems.forEach((imgData) => {
+  // Version with JS
+  const img = createElement(imgData.preview, imgData.original, imgData.description);
+  gallery.append(img);
+
+  // Version with concat
+  //const img = createDiv(imgData.preview, imgData.original, imgData.description);
+  //gallery.insertAdjacentHTML("afterbegin", img);
+});
+
+// Version with JS
+// Create box of image
+function createElement(preview, original, description) {
+  const div = document.createElement("div");
+  const link = document.createElement("a");
+  const img = document.createElement("img");
+  div.classList.add("gallery__item");
+  link.classList.add("gallery__link");
+  link.setAttribute("href", original);
+  img.classList.add("gallery__image");
+  img.setAttribute("src", preview);
+  img.setAttribute("data-source", original);
+  img.setAttribute("alt", description);
+
+  link.appendChild(img);
+  div.appendChild(link);
+  return div;
+}
+
+// Version with concat(Uncomment strings above)
+function createDiv(preview, original, description) {
+  const div = `<div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>`;
+  return div;
+}
+
+// Events on img preview
+gallery.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName == "IMG") {
+    const imgTarget = e.target;
+    initImgBox(imgTarget);
+    showImage();
+  }
+});
+
+// Init lightBox
+function initImgBox(imgTarget) {
+  const src = imgTarget.getAttribute("data-source");
+  imgBox = basicLightbox.create(`<img src="${src}"></img>`, {
+    onClose: () => {
+      gallery.removeEventListener("keydown", closeByEsc);
+    },
+    onShow: () => {
+      gallery.addEventListener("keydown", closeByEsc);
+    },
+  });
+}
+
+// Show image on click
+function showImage() {
+  imgBox.show();
+}
+
+// Close imgBox by Esc
+function closeByEsc(e) {
+  if (e.key == "Escape") {
+    imgBox.close();
+  }
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+var lightbox = new SimpleLightbox(".gallery", {
+  /* options */
+});
+
+// Append all images from array to html
+galleryItems.forEach((imgData) => {
+  const img = createElement(imgData.preview, imgData.original, imgData.description);
+  gallery.append(img);
+});
+
+// Version with JS
+// Create box of image
+function createElement(preview, original, description) {
+  const link = document.createElement("a");
+  const img = document.createElement("img");
+  link.classList.add("gallery__item");
+  link.setAttribute("href", original);
+
+  img.classList.add("gallery__image");
+  img.setAttribute("src", preview);
+  img.setAttribute("alt", description);
+
+  link.appendChild(img);
+  return link;
+}
+
+console.log(lightbox);
+
+lightbox.on('show.simplelightbox', function () {
+	// do something…
+    console.log('show');
+});
